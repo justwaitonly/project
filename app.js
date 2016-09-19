54a6baf806da319e6eff8067e0027b476351dab1
huangfang <huangfang@123.com>
2016-09-19 22:03:44 +0800

    project init

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Administrator on 2016/9/3.
+ */
+//主入口模块
+
+'use strict'
+
+angular.module('appModule',[
+ 'config',
+    'ui.router',
+    'listModule.controller',
+    'detailModule.controller',
+
+])
+
+.controller('appController',['$scope',function($scope){
+
+    }])
+
+.config(['$stateProvider','urlRouteProvider',function($stateProvider,urlRouteProvider){
+
+        $stateProvider
+            .state('list',{
+                url:"/list:type",
+                templateUrl:'areas/list/list_controller.html',
+                controller:'listController'
+            })
+            .state('detail',{
+                url:"/detail:type",
+                templateUrl:'./areas/detail/detail.html',
+                controller:'detailController'
+            })
+
+        $UrlRouterProvider.otherwise("/list/in_theaters");
+    }])
